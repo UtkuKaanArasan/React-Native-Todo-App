78f75913383d8d667f0d9788eba743c8105b89d7
UtkuKaanArasan <utkukaan000@protonmail.ch>
2022-06-04 20:28:49 +0300

    inital commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList, Alert } from "react-native";
+import AddItem from "./Components/AddItem";
+import Header from "./Components/Header";
+import ListItem from "./Components/ListItem";
+
+const App = () => {
+
+  const [items, setItems] = useState([
+    { id: Math.floor(Math.random() * 255), text: 'milk' },
+    { id: Math.floor(Math.random() * 255), text: 'meat' },
+    { id: Math.floor(Math.random() * 255), text: 'egg' },
+    { id: Math.floor(Math.random() * 255), text: 'bread' },
+    { id: Math.floor(Math.random() * 255), text: 'chocolate' },
+  ])
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id)
+    })
+  }
+
+  const addItem = (newItemText) => {
+    if (!newItemText) {
+      Alert.alert("Error", "Please enter a item")
+    } else {
+      setItems((prevItems) => {
+        return [...prevItems, {id: Math.floor(Math.random() * 255), text: newItemText}]
+      })
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={(item) => addItem(item)} />
+      <FlatList
+        data={items}
+        renderItem={
+          ({ item }) => {
+            return (
+              <ListItem item={item} deleteItem={deleteItem}/>
+            )
+          }
+        }
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
+
+export default App;
---
+++ Components/AddItem.js
@@ -0,0 +1,66 @@
+import React, { useRef } from "react";
+import {
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    TouchableOpacity,
+    Keyboard
+} from "react-native";
+import { useState } from "react";
+import Icon from "react-native-vector-icons/dist/FontAwesome";
+
+const AddItem = ({addItem}) => {
+
+    const [userInput, setUserInput] = useState('')
+
+    const inputRef = useRef()
+
+    function onChange(textValue) {
+        setUserInput(textValue)
+    }
+
+    function submitHandler() {
+        addItem(userInput)
+        Keyboard.dismiss()
+        setUserInput('')
+    }
+
+    return (
+        <View>
+            <TextInput 
+                ref={inputRef}
+                style={styles.input} 
+                placeholder='Add item...'
+                onChangeText={onChange}
+                value={userInput}
+            />
+            <TouchableOpacity style={styles.btn}>
+                <Text onPress={submitHandler} style={styles.btnText}>
+                    <Icon name='plus' size={20} />
+                    Add Item
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 60,
+        padding: 8,
+        fontSize: 16,
+    },
+    btn: {
+        backgroundColor: 'darkslateblue',
+        padding: 9,
+        margin:5
+    },
+    btnText: {
+        color: 'white',
+        fontSize: 20,
+        textAlign:'center',
+    },
+})
+
+export default AddItem;
